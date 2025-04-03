84d2fc5cd772c4f93a8f5f1c09c3c321952c2ad0
Ethan Sie <esie50@stuy.edu>
2025-04-03 08:01:14 -0400

    some basic website workings

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,72 @@
-from flask import Flask
+# JEWZ
+# SoftDev
+# P04:
+# 2025-04-XX
+# Time Spent: not enough hours
+
+import random
+import datetime
+import os
+import sqlite3
+import sys
+from flask import Flask, render_template, request, session, redirect, url_for, flash, jsonify
+import json
+
+DB_FILE = "db.py"
 app = Flask(__name__)
+app.secret_key = os.urandom(32)
+anchor = False
 
-@app.route("/")
-def hello():
-    return "<h1 style='color:black'>do i have to sudo restart everytime lol  WSHOW UP DATES NOW PLEASEASUGFIUESGFUISGRHFUISGRIUFBPLEASE WOROOOK I love that this works!! Hello There!</h1>"
+@app.route("/", methods=['GET', 'POST'])
+def home():
+    return render_template("home.html")
 
-if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0', port=3000)
+@app.route('/register', methods=['GET','POST'])
+def register():
+    if not request.form:
+        flash("You must use the menu to register", 'error')
+        return redirect("/")
+    else:
+        username = request.form['username']
+        password = request.form['password']
+        password2 = request.form.get('password2')
+
+        if password != password2:
+            flash("Passwords do not match", 'error')
+            return redirect("/")
+        elif db.getUserID(username) >= 0:
+            flash("Username already exists", 'error')
+            return redirect("/")
+        else:
+            session['username'] = username
+            active_sessions[session['username']] = db.getUserID(session['username'])
+            flash("Registered Sucessfully!", "success")
+            db.addUser(username, password)
+            return redirect("/")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if not request.form:
+        flash("You must use the menu to log in", 'error')
+        return redirect("/")
+    else:
+        username = request.form['username']
+        password = request.form['password']
+        if username in active_sessions:
+            flash("You already have an active session.", 'error')
+        elif db.getUserID(username) >= 0 and db.getTableData("users", "username", username)[2] == password:
+            session['username'] = username
+            active_sessions[session['username']] = db.getUserID(session['username'])
+            db.updateLoginTime(session['username'])
+            flash("Logged in", 'success')
+        else:
+            flash("Incorrect username or password.", 'error')
+        return redirect("/")
+
+@app.route("/logout", methods=['GET', 'POST'])
+def logout():
+    if 'username' in session:
+        flash("Logged out", 'success')
+        active_sessions.pop(session['username'])
+        session.pop('username', None)
+    return redirect("/")
