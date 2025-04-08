0e87fdcdc55c951ca7773217dc1f1d9dddba4d7a
Ethan Sie <esie50@stuy.edu>
2025-04-08 10:43:19 -0400

    new csv and more interactions

--- app/__init__.py
+++ app/__init__.py
@@ -24,10 +24,24 @@ if (not os.path.isfile("stock.db")):
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
+    sp500Stocks = pd.read_csv('csv/all_stocks_5yr.csv')
+
+    stockNames = sp500Stocks['Name'].unique()
+
     passValue = 'username' in session
     if 'username' in session:
-        return render_template("home.html", logged_in = passValue, username=session['username'])
+        return render_template("home.html", logged_in = passValue, username=session['username'], stockNames = stockNames)
     return render_template("home.html", logged_in = passValue)
+
+@app.route("/view_stock", methods=['GET','POST'])
+def view():
+    stockName = request.form['stock']
+    sp500Stocks = pd.read_csv('csv/all_stocks_5yr.csv')
+
+    stockDates = sp500Stocks[sp500Stocks['Name'] == stockName]['date'].tolist()
+    stockHigh = sp500Stocks[sp500Stocks['Name'] == stockName]['high'].tolist()
+
+    return render_template("home.html", logged_in = 'username' in session, username = session['username'], stockDates = stockDates, stockHigh = stockHigh)
 
 @app.route("/portfolio", methods=['GET', 'POST'])
 def analysis():
@@ -47,11 +61,14 @@ def list():
 
     dates = index['Date'].tolist()
     sp = index['S&P500'].tolist()
-    # Exchange,Symbol,Shortname,Longname,Sector,Industry,Currentprice,Marketcap,Ebitda,Revenuegrowth,City,State,Country,
-    print(companies['Shortname'])
-    print(companies.nlargest(10, 'Currentprice'))
+    # # Exchange,Symbol,Shortname,Longname,Sector,Industry,Currentprice,Marketcap,Ebitda,Revenuegrowth,City,State,Country,
+    # print(companies['Shortname'])
+    # print(companies.nlargest(10, 'Currentprice'))
 
-    return render_template("stock_list.html", data1=dates, data2=sp, logged_in = passValue)
+    sp500Stocks = pd.read_csv('csv/all_stocks_5yr.csv')
+    stockNames = sp500Stocks['Name'].unique()
+
+    return render_template("stock_list.html", data1=dates, data2=sp, logged_in = passValue, stockNames = stockNames)
 
 @app.route('/register', methods=['GET','POST'])
 def register():
